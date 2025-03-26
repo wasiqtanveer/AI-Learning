f20b41bf87860f1f97d0ffdfa3ea5eeb99c35996
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-03-27 00:03:00 +0500

    new problems

---
+++ Python/Basics/Chapter 7 (Loops)/problems/problem_1.py
@@ -0,0 +1,4 @@
+table = int(input("Ente rthe number you want to get the table for: "))
+
+for i in range(1,11):
+    print(table , "x" , i , "=" ,table*i)
---
+++ Python/Basics/Chapter 7 (Loops)/problems/problem_2.py
@@ -0,0 +1,6 @@
+lists = ["saqib" , "naveed" , "saeed" , "iftikhar"]
+
+for i in lists:
+    if i.startswith("s"):
+        print(i)
+    
