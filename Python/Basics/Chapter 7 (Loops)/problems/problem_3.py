88fc201c6c925bc2f9aa8c7d703c8a94233de9a6
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-03-27 12:24:20 +0500

    new problems

--- Python/Basics/Chapter 7 (Loops)/problems/problem_3.py
+++ Python/Basics/Chapter 7 (Loops)/problems/problem_3.py
@@ -1,2 +1,21 @@
 number  = int(input("Enter a number: "))
 
+prime = True
+
+if number < 2:
+    prime = False
+
+elif number == 2:
+    prime = True
+
+else:
+    for i in range(2,number):
+        if (number % i == 0):
+            prime = False
+            break
+
+if prime:
+    print(number,": Is a prime Number")
+
+else:
+    print(number,": Is not a prime Number")
---
+++ Python/Basics/Chapter 7 (Loops)/problems/problem_4.py
@@ -0,0 +1,6 @@
+number  = int(input("enter the number to find sum till: "))
+sum = 0
+for i in range(number+1):
+    sum = sum+i
+
+print(sum)
