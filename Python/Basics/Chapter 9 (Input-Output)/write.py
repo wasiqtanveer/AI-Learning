8d69fba85162aa7454da7b5cab27b9db1fd148c2
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-03-31 22:04:02 +0500

    new problems

---
+++ Python/Basics/Chapter 9 (Input-Output)/write.py
@@ -0,0 +1,4 @@
+string = "My name is wasiq tanverr and i am making this file through python"
+files = open("Name.txt", "w") # opens the file in write mode
+files.write(string) # writes the string to the file
+files.close() # closes the file
