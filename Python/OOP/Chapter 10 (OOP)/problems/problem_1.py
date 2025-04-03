1dee0dcf589ae3dac10877070ff1f200f08f23d9
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-03 22:36:29 +0500

    new problems

--- Python/OOP/Chapter 10 (OOP)/problems/problem_1.py
+++ Python/OOP/Chapter 10 (OOP)/problems/problem_1.py
@@ -1,3 +1,5 @@
+""""
+Problem 1: Create a class calculator with the following methods:
 class calculator:
 
     @staticmethod   
@@ -24,3 +26,33 @@ print("Did using class name instead of object name")
 print(calculator.square(5))
 print(calculator.cube(2))
 print(calculator.under_root(16))  
+"""
+class calculator:
+    def __init__(self,number):
+        self.number = number
+        
+    def square(self):
+        square = pow(self.number, 2)
+        # return f"Square of {self.number} is {square}" , square
+        return square
+    
+    def cube(self):
+        cube = pow(self.number, 3)            
+        # return f"Cube of {self.number} is {cube}" , cube
+        return cube
+    
+    def under_root(self):
+        root = pow(self.number, 0.5)
+        # return f"Square root of {self.number} is {root}", root
+        return root
+    
+    def __str__(self):
+        return f"""
+Square    : {self.square()}
+Cube      : {self.cube()}
+Under Root: {self.under_root()}
+"""
+
+number1 = calculator(16)
+print(number1)  
+print()
