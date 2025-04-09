1c288d3e26fba44ddc98b919ee1bf6e14592cee2
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-09 21:49:57 +0500

    new problems

---
+++ Python/OOP/Chapter 12 (Advanced Python)/exception_handling.py
@@ -0,0 +1,33 @@
+# number = int(input("Enter a number: "))
+# print("Number is: ", number) 
+
+try:
+    number = int(input("Enter a number: "))
+
+except ValueError as e:
+    print("An exception occurred: ", e)
+# except Exception as e:
+#     print("An exception occurred: ", e) # program won't crash
+    # Handle the exception
+    
+# ================= different exception handlers =======================
+
+
+# zero division error
+try:
+    a = 10/0
+    
+except ZeroDivisionError as e:
+    print("An exception occurred: ", e)
+    
+    
+# type error
+
+try:
+    # a = "5" + 3
+    print(a)
+    
+except TypeError as e:
+    print("An exception occurred: ", e)
+    
+    
