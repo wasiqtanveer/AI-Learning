7186d99ce99989b3a980703103f5d900975fa87f
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-10 22:58:29 +0500

    new problems

--- Python/OOP/Chapter 12 (Advanced Python)/module/main.py
+++ Python/OOP/Chapter 12 (Advanced Python)/module/main.py
@@ -4,4 +4,4 @@ def name(name):
 name("wasiq")
 print(f"code is being ran from : {__name__}")
 # "if __main__ is printed then it means this code is being run from main.py"
-# "anything means this code is being run from other.py
+# "anything means this code is being run from other.p
