086f4c9327c969cebb2117061fe94061ec3c9343
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-11 22:29:28 +0500

    new problems

---
+++ Python/OOP/Chapter 12 (Advanced Python)/problems/problem_2.py
@@ -0,0 +1,5 @@
+list1 = [1,2,3,4,5,6,7,8,9]
+
+for index,value in enumerate(list1): #first variable that is index will give us the value of index and seecond variable will give us value
+    if index == 2 or index == 4 or index == 6:
+        print(value)
