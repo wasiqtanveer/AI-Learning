6673a4b8219061dd37c1d8c76e2c0011c3ade6e8
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-12 22:31:25 +0500

    new problems

--- Python/OOP/Chapter 12 (Advanced Python)/problems/problem_5.py
+++ Python/OOP/Chapter 12 (Advanced Python)/problems/problem_5.py
@@ -2,6 +2,6 @@ table = int(input("Enter a number : "))
 
 list2 = [(i*table) for i in range(1,11)]
 
-with open("tables.txt","w") as table:
-    for i in list2:
-        table.write(f"{str(i)}")
+with open("tables.txt","w") as table: 
+    for i in list2: 
+        table.write(f"{str(i)}") 
