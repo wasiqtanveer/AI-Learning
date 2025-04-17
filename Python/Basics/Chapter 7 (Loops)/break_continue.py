fef99dd7d918296cca5d482ff58791cb4ffc3a65
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-04-17 22:22:22 +0500

    new problems

--- Python/Basics/Chapter 7 (Loops)/break_continue.py
+++ Python/Basics/Chapter 7 (Loops)/break_continue.py
@@ -7,7 +7,7 @@ for i in range(10):
     if (i == 5 or i == 2 or i == 3):
           
         continue # as i = 5  skip the number and continue    
-    print(i)   
+    print(i)       
     # ============ using pass in loop  
     for i in range(10):  
         pass #in case you want to populate this section later or not at all, without pass it will throw error
